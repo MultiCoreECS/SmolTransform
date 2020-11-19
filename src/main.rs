672aa2ecb398a1d8830a91c14b289be01c806072
Blake Wyatt <nabakin@protonmail.ch>
2020-11-19 02:14:22 -0500

    Add command line usage and random object creation

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use SmolECS::{
     world::*,
 };
 use std::sync::{Arc, Mutex};
+use clap::*;
+use rand::*;
 
 #[derive(Copy, Clone)]
 pub struct Position {
@@ -31,7 +33,7 @@ pub struct Time {
     delta: f64
 }
 
-pub struct UpdateTime;
+/*pub struct UpdateTime;
 impl<'d, 'w: 'd> System<'d, 'w, World> for UpdateTime {
     type SystemData = (
         Write<'d, Time>
@@ -43,26 +45,43 @@ impl<'d, 'w: 'd> System<'d, 'w, World> for UpdateTime {
         time.total = current.duration_since(time.beginning).as_secs_f64();
         time.last = current;
     }
-}
+}*/
 
 pub struct ApplyRotationalVelocities;
 impl<'d, 'w: 'd> System<'d, 'w, World> for ApplyRotationalVelocities{
     type SystemData = (
         ReadComp<'d, RotationalVelocity>,
-        Read<'d, Time>,
+        //Read<'d, Time>,
         WriteComp<'d, Rotation>
     );
 
-    fn run(&self, (vels, time, mut rots): Self::SystemData) {
+    fn run(&self, (vels, mut rots): Self::SystemData) {
         for (vel, rot) in (&vels, &mut rots).join(){
-            rot.0 += vel.0 * time.delta as f32;
+            rot.0 += vel.0;// * time.delta as f32;
             rot.0 = rot.0.signum() * rot.0.abs() % 360.0;
         }
     }
 }
 
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("SmolTransform")
+        .version("1.0")
+        .author("Blake Wyatt")
+        .about("A transform hierarchy experiment")
+        .arg(Arg::with_name("object_count")
+            .short("c")
+            .long("object_count")
+            .help("Sets the number of objects to generate")
+            .takes_value(true))
+        .arg(Arg::with_name("update_iterations")
+            .short("i")
+            .long("update_iterations")
+            .help("Sets the number of transform update iterations to perform")
+            .takes_value(true))
+        .get_matches();
+
+    let object_count = matches.value_of("object_count").unwrap_or("10").parse::<i32>().unwrap_or(10);
+    let update_iterations = matches.value_of("update_iterations").unwrap_or("100000").parse::<i32>().unwrap_or(100000);
 
     let mut world = World::new();
     world.register_comp::<Position>();
@@ -81,27 +100,25 @@ fn main() {
     let mut positions = WriteComp::<Position>::get_data(&world);
     let mut angles = WriteComp::<Rotation>::get_data(&world);
     let mut angle_vel = WriteComp::<RotationalVelocity>::get_data(&world);
-
-    ents.create_entity()
-        .add(&mut positions, Position{x: 1.0, y: 2.0})
-        .add(&mut angles, Rotation(100.0))
-        .add(&mut angle_vel, RotationalVelocity(1.2));
     
-    ents.create_entity()
-        .add(&mut positions, Position{x: 0.0, y: -5.0})
-        .add(&mut angles, Rotation(359.9))
-        .add(&mut angle_vel, RotationalVelocity(10.0));
+    let mut rng = rand::thread_rng();
+    for _ in 0..object_count {
+        ents.create_entity()
+            .add(&mut positions, Position{x: rng.gen_range(0.0, 100.0), y: rng.gen_range(0.0, 100.0)})
+            .add(&mut angles, Rotation(rng.gen_range(0.0, 360.0)))
+            .add(&mut angle_vel, RotationalVelocity(rng.gen_range(0.0, 1.0)));
+    }
     
     let mut scheduler = SystemScheduler::new(Arc::new(ThreadPoolBuilder::new().num_threads(4).build().unwrap()));
-    scheduler.add(UpdateTime{}, "update_time", vec![]);
-    scheduler.add(ApplyRotationalVelocities{}, "update_angles", vec!["update_time"]);
+    //scheduler.add(UpdateTime{}, "update_time", vec![]);
+    scheduler.add(ApplyRotationalVelocities{}, "update_angles", vec![]);//"update_time"]);
 
     drop(ents);
     drop(positions);
     drop(angles);
     drop(angle_vel);
 
-    for _ in 0..100 {
+    for _ in 0..update_iterations {
         scheduler.run(&world);
     }
 
